2f74878101e80a817e02b0ca5c4d6ffa4d3d2c43
BalazsBano <rhonadler@gmail.com>
2023-03-25 13:42:49 +0100

    Create basic express server with mysql connections

---
+++ backend/src/index.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import mysql from "mysql";
+const app = express();
+
+var port = 4000;
+var userId = 1;
+var newNumber = 4;
+
+var con = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  database: "Calculator",
+  user: "root",
+  password: "password"
+});
+
+con.connect(function(err) {
+  if(err) throw err;
+  console.log("Connected!");
+});
+
+app.get('/', (req, res) => {
+  con.query(`SELECT * FROM Memory WHERE idMemory=${userId}`, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+    res.send(result);
+  });
+})
+
+app.post('/', (req, res) => {
+  con.query(`UPDATE Memory SET value = ${newNumber} WHERE idMemory = ${userId}`, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record updated");
+    res.send(result);
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
